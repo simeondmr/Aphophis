33853570ebe75b6975f4bfa41b5304fec85877d7
Simeon Tornabene <simeon.tornabene@rcslab.it>
2022-09-18 18:56:36 +0200

    Implemented DPS310's driver

---
+++ README.md
@@ -0,0 +1,2 @@
+# Aphophis
+Aphophis drone
---
+++ main/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "esp_system.h"
+#include "driver_dps310.h"
+#include "driver/i2c.h"
+#include "esp_log.h"
+
+extern "C" {
+    void app_main(void);
+}
+
+
+void app_main(void)
+{
+    
+}
